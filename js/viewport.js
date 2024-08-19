9d430f397d0b8cb7396e12160328213d1d20f05c
weilin97462 <72684127+weilin97462@users.noreply.github.com>
2024-08-19 14:18:32 +0800

    Update viewport.js

--- js/viewport.js
+++ js/viewport.js
@@ -3,7 +3,7 @@ const desiredWidth = 600;
 // Get the actual device width
 const deviceWidth = window.screen.width;
 // Calculate the initial scale
-const initialScale = deviceWidth / desiredWidth;
+const initialScale = Math.min(deviceWidth / desiredWidth, 1.0);
 // Update the viewport meta tag
 const viewportMeta = document.querySelector("meta[name=viewport]");
 viewportMeta.setAttribute('content', `width=${desiredWidth}, initial-scale=${initialScale}`);
